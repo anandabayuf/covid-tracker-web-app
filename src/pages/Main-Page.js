0a90be3de8005bc93b2bad935e0eb4678505ac88
Ananda Bayu <anandabayu090@gmail.com>
2022-09-17 23:52:43 +0700

    add confirmed & Yaqub

--- src/pages/Main-Page.js
+++ src/pages/Main-Page.js
@@ -3,19 +3,26 @@ import confirmed from "../assets/icons/confirmed.png"
 import deaths from "../assets/icons/deaths.png"
 import recovered from "../assets/icons/recovered.png"
 import { useEffect, useState } from "react";
+import { getSummary } from "../providers/Summary";
 
 export default function MainPage () {
     const [data, setData] = useState({});
+    
+    const getData = async () => {
+        const response = await getSummary();
+        console.log(response)
+        setData(response)
+    }
 
     useEffect(() => {
-
+        getData()
     }, []);
 
     return (
         <div className="container">
             <div className="row">
                 <div className="col-4">
-                    <CardSummary image={confirmed} title="Total Confirmed" data="1.000.000" />
+                    <CardSummary image={confirmed} title="Total Confirmed" data={JSON.stringify(data.confirmed.value) || ""} />
                 </div>
                 <div className="col-4">
                     <CardSummary image={recovered} title="Total Recovered" data="1.000.000" />
