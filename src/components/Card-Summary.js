a3155ee8350b3f612f40c6f988d13370e0ead05f
Ananda Bayu <anandabayu090@gmail.com>
2022-09-17 23:07:29 +0700

    add card style -notdone

--- src/components/Card-Summary.js
+++ src/components/Card-Summary.js
@@ -1,18 +1,21 @@
-
 export default function CardSummary(props) {
+    const cardStyle = {
+        maxWidth: "300px"
+    }
+
     return (
-        <div className="card mb-3" style={{maxWidth: '300px'}} >
-        <div className="row g-0">
-            <div className="col-md-4">
-            <img src={props.image} className="img-fluid rounded-start" alt="..." width="128px" />
+        <div className="card mb-3 shadow" style={cardStyle}>
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src={props.image} className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">{props.title}</h5>
+                        <h3 className="card-text text-end">{props.data}</h3>
+                    </div>
+                </div>
             </div>
-            <div className="col-md-8">
-            <div className="card-body">
-                <h5 className="card-title">{props.title}</h5>
-                <h3 className="card-text text-end">{props.data}</h3>
-            </div>
-            </div>
-        </div>
         </div>
     )
 }
--- src/pages/Main-Page.js
+++ src/pages/Main-Page.js
@@ -2,21 +2,28 @@ import CardSummary from "../components/Card-Summary"
 import confirmed from "../assets/icons/confirmed.png"
 import deaths from "../assets/icons/deaths.png"
 import recovered from "../assets/icons/recovered.png"
+import { useEffect, useState } from "react";
 
 export default function MainPage () {
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+
+    }, []);
 
     return (
-        <div className="row mr-2">
-            <div className="col-3 mr-2">
-            <CardSummary image={confirmed} title="Total Confirmed" data="1.000.000" />
-            </div>
-            <div className="col-3">
-            <CardSummary image={recovered} title="Total Recovered" data="1.000.000" />
-            </div>
-            <div className="col-3">
-            <CardSummary image={deaths} title="Total Deaths" data="1.000.000" />
+        <div className="container">
+            <div className="row">
+                <div className="col-4">
+                    <CardSummary image={confirmed} title="Total Confirmed" data="1.000.000" />
+                </div>
+                <div className="col-4">
+                    <CardSummary image={recovered} title="Total Recovered" data="1.000.000" />
+                </div>
+                <div className="col-4">
+                    <CardSummary image={deaths} title="Total Deaths" data="1.000.000" />
+                </div>
             </div>
         </div>
-        
     )
 }
