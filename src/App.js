c7e7ebbab79f4eb5cb61798822eb2067347e6b83
Ananda Bayu <anandabayu090@gmail.com>
2022-09-18 16:44:52 +0700

    fix merging

--- src/App.js
+++ src/App.js
@@ -7,21 +7,8 @@ import {
 import MainPage from "./pages/Main-Page";
 import NavBar from "./components/Nav-Bar";
 
-
-
 function App(){
   return (
-<<<<<<< HEAD
-    <div>
-<<<<<<< HEAD
-      <Navigasi />
-=======
-      <NavBar />
->>>>>>> 0c8887d95eed46b908890cfeae326406030ec862
-      <MainPage />
-    </div>
-  );
-=======
     <BrowserRouter>
       <NavBar />
       <Routes>
@@ -33,7 +20,6 @@ function App(){
       </Routes>
     </BrowserRouter>
   )
->>>>>>> 843bfd2b56539d1db337cab8ed962fc3fecdd91c
 }
 
 export default App;
