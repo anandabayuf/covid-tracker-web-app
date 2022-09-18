e9c5d38e96daa278a4b845cbee04aab30188a007
RizkaAgustin <rizkaagustin393@gmail.com>
2022-09-18 15:54:49 +0700

    membuat navigation mainpage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import MainPage from "./pages/Main-Page";
-import NavBar from "./components/Nav-Bar";
+import { render } from "react-dom";
+import App from './App';
+//import NavBar from "./components/Nav-Bar";
+//import ReactDOM from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <MainPage />
-    </div>
-  );
-}
+render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />}>
+        {/* Tolong review mas. <Route index element={<About />}>
+          </Route>*/}
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
 
 export default App;
+
